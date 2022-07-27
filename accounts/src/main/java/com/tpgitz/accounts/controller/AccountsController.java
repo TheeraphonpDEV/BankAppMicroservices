d8594480130543b9f466e2283189d55247a8ed7b
TPGitz <103758766+TheeraphonpDEV@users.noreply.github.com>
2022-07-27 18:15:59 +0700

    process - 3
    1. add serverconfig
    2. add config file for accounts, loans, cards
    3. docker compose default, dev, prod

--- accounts/src/main/java/com/tpgitz/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/tpgitz/accounts/controller/AccountsController.java
@@ -1,23 +1,30 @@
-/**
- *
- */
+
 package com.tpgitz.accounts.controller;
 
+
+import com.tpgitz.accounts.config.AccountsServiceConfig;
+import com.tpgitz.accounts.model.Accounts;
+import com.tpgitz.accounts.model.Customer;
+import com.tpgitz.accounts.model.Properties;
+import com.tpgitz.accounts.repository.AccountsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.tpgitz.accounts.model.Accounts;
-import com.tpgitz.accounts.model.Customer;
-import com.tpgitz.accounts.repository.AccountsRepository;
-
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 
 @RestController
 public class AccountsController {
 
     @Autowired
     private AccountsRepository accountsRepository;
+
+    @Autowired
+    AccountsServiceConfig accountsConfig;
 
     @PostMapping("/myAccount")
     public Accounts getAccountDetails(@RequestBody Customer customer) {
@@ -29,6 +36,15 @@ public class AccountsController {
             return null;
         }
 
+    }
+
+    @GetMapping("/account/properties")
+    public String getPropertyDetails() throws JsonProcessingException {
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        Properties properties = new Properties(accountsConfig.getMsg(), accountsConfig.getBuildVersion(),
+                accountsConfig.getMailDetails(), accountsConfig.getActiveBranches());
+        String jsonStr = ow.writeValueAsString(properties);
+        return jsonStr;
     }
 
 }
--- cards/src/main/java/com/tpgitz/cards/controller/CardsController.java
+++ cards/src/main/java/com/tpgitz/cards/controller/CardsController.java
@@ -1,18 +1,21 @@
-/**
- *
- */
-package com.tpgitz.cards.controller;
 
+package com.tpgitz.cards.controller;
 
 import java.util.List;
 
+import com.tpgitz.cards.config.CardsServiceConfig;
 import com.tpgitz.cards.model.Cards;
 import com.tpgitz.cards.model.Customer;
+import com.tpgitz.cards.model.Properties;
 import com.tpgitz.cards.repository.CardsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 
 
 @RestController
@@ -20,6 +23,9 @@ public class CardsController {
 
     @Autowired
     private CardsRepository cardsRepository;
+
+    @Autowired
+    CardsServiceConfig cardsConfig;
 
     @PostMapping("/myCards")
     public List<Cards> getCardDetails(@RequestBody Customer customer) {
@@ -30,6 +36,15 @@ public class CardsController {
             return null;
         }
 
+    }
+
+    @GetMapping("/cards/properties")
+    public String getPropertyDetails() throws JsonProcessingException {
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        Properties properties = new Properties(cardsConfig.getMsg(), cardsConfig.getBuildVersion(),
+                cardsConfig.getMailDetails(), cardsConfig.getActiveBranches());
+        String jsonStr = ow.writeValueAsString(properties);
+        return jsonStr;
     }
 
 }
