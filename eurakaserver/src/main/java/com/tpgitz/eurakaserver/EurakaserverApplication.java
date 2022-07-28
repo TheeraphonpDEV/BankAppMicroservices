9072cd8348d4533535eb0f02dd58a13a3e33efaa
TPGitz <103758766+TheeraphonpDEV@users.noreply.github.com>
2022-07-29 01:27:13 +0700

    process - 4
    create eurakaserver

---
+++ eurakaserver/src/main/java/com/tpgitz/eurakaserver/EurakaserverApplication.java
@@ -0,0 +1,13 @@
+package com.tpgitz.eurakaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurakaserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurakaserverApplication.class, args);
+	}
+
+}
