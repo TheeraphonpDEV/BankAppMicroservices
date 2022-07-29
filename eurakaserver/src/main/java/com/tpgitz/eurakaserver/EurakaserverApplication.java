cd098882b5f8e47750528113bf4905befa23aeea
TPGitz <103758766+TheeraphonpDEV@users.noreply.github.com>
2022-07-29 15:34:45 +0700

    process - 5
    updated eureka server config

--- eurakaserver/src/main/java/com/tpgitz/eurakaserver/EurakaserverApplication.java
+++ eurakaserver/src/main/java/com/tpgitz/eurakaserver/EurakaserverApplication.java
@@ -2,12 +2,14 @@ package com.tpgitz.eurakaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-public class EurakaserverApplication {
+@EnableEurekaServer
+public class EurekaserverApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EurakaserverApplication.class, args);
+		SpringApplication.run(EurekaserverApplication.class, args);
 	}
 
 }
